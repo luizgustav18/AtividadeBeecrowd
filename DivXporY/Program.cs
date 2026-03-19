73b46f8c8d3e5cd031052561203260c5e34d1b2b
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:22:00 -0300

    commit

---
+++ DivXporY/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        int X = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite outro valor: ");
+        int Y = int.Parse(Console.ReadLine());
+
+        if (Y == 0)
+        {
+            Console.WriteLine("Não é possível dividir por zero!");
+        }
+        else
+        {
+            double div = (double)X / Y;
+            Console.WriteLine("Sua divisão: " + div);
+        }
+    }
+}
---
+++ Divisores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            if (N % i == 0)
+            {
+                Console.WriteLine("Os divisor do número: " + i);
+            }
+        }
+    }
+}
