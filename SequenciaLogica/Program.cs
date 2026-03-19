456c138da73355a22c64e4b33467782560ff4091
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:27:59 -0300

    commit

---
+++ SequenciaLogica/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            double quadrado = Math.Pow(i, 2);
+            double cubo = Math.Pow(i, 3);
+            Console.WriteLine("Primeira linha: " + i + " " + quadrado + " " + cubo + " ");
+            Console.WriteLine("Segunda linha: " + i + " " + (quadrado + 1) + " " + (cubo + 1));
+        }
+    
+    }
+}
---
+++ SequenciadeIeJ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int I = 1; I <= 9; I += 2)
+        {
+            for (int J = 7; J >= 5; J--)
+            {
+                Console.WriteLine("I=" + I + " J=" + J);
+            }
+        }
+    }
+}
