b03c9cbcf5549598342517c0616f426dc6ab1ee7
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:32:06 -0300

    commit

---
+++ Atividadebeec02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        int X = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite outro valor: ");
+        int Y = int.Parse(Console.ReadLine());
+
+        int soma = 0;
+
+        int menor = Math.Min(X, Y);
+        int maior = Math.Max(X, Y);
+
+        for (int i = menor + 1; i < maior; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma += i;
+            }
+        }
+
+        Console.WriteLine("A soma dos valores: " + soma);
+    }
+}
---
+++ Atividadebeec03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Quantos números você vai digitar: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int dentro = 0;
+        int fora = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            Console.Write("Digite um número: ");
+            int X = int.Parse(Console.ReadLine());
+
+            if (X >= 10 && X <= 20)
+            {
+                dentro++;
+            }
+            else
+            {
+                fora++;
+            }
+        }
+
+        Console.WriteLine(dentro + " estão");
+        Console.WriteLine(fora + " Não estão");
+    }
+}
