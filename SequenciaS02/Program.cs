f6f8d82893008a01a0a4cb77f10a9534be094f1e
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:29:06 -0300

    commit

---
+++ SequenciaS02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double S = 0.0;
+        int numerador = 1;
+        int denominador = 1;
+
+        for (int i = 0; i < 20; i++)
+        {
+            S += (double)numerador / denominador;
+
+            numerador += 2;
+            denominador *= 2;
+        }
+
+        Console.WriteLine(S.ToString("F2"));
+    }
+}
