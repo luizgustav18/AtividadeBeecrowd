e197ee5b9c569cd0931b37d30570607a34ebf992
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:31:08 -0300

    commit

---
+++ Atividadebeecrowd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        int X = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= X; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine("Oa números impares a seguir: " + i);
+            }
+        }
+    }
+}
