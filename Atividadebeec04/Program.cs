1e2c7c1ab2c32dd96baa01571bea10a4896327b2
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:35:16 -0300

    commit

---
+++ Atividadebeec04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        int X = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= X; i++)
+        {
+            if (i % 2 == 0)
+            {
+                double quadrado = Math.Pow(i,2);
+                Console.WriteLine("O quadrado do números pares " + i + " é: " + quadrado);
+            }
+        }
+    }
+}
---
+++ Atividadebeec05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+            if (X % 2 == 0)
+            {
+                Console.Write("EVEN ");
+            }
+            else
+            {
+                Console.Write("ODD ");
+            }
+
+            if (X > 0)
+            {
+                Console.WriteLine("POSITIVE");
+            }
+            else if(X ==0 )
+            {
+                Console.WriteLine("NULL");
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVE");
+            }
+        }
+    }
+}
