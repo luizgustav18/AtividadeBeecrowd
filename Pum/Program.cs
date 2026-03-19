105a221e765252efa45ab39b56c7c1161f39e796
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:26:09 -0300

    commit

---
+++ Pum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um valor: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int contador = 1;
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine($"{contador} {contador + 1} {contador + 2} PUM");
+            contador += 4;
+        }
+    }
+}
---
+++ QuadradoeCubo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+    {
+        Console.WriteLine("Digite um valor: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            double quadrado = Math.Pow (i,2);
+            double cubo = Math.Pow (i, 3);
+
+            Console.WriteLine(i + " " + quadrado + " " + cubo);
+        }
+    }
+}
