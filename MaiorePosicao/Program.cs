76405ec1a2a0adc1949e6cbda25a1c12f5e7a15d
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:25:19 -0300

    commit

---
+++ MaiorePosicao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maior = int.MinValue;
+        int posicao = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.Write("Digite um valor: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            if (valor > maior)
+            {
+                maior = valor;
+                posicao = i;
+            }
+        }
+
+        Console.WriteLine("O maior numero escrito: " + maior);
+        Console.WriteLine("Sua posição: " + posicao);
+    }
+}
---
+++ Multiplosde13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um valor: ");
+        int X = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite um valor: ");
+        int Y = int.Parse(Console.ReadLine());
+
+        int inicio = Math.Min(X, Y);
+        int fim = Math.Max(X, Y);
+
+        int soma = 0;
+
+        for (int i = inicio; i <= fim; i++)
+        {
+            if (i % 13 != 0)
+            {
+                soma += i;
+            }
+        }
+
+        Console.WriteLine("Sua soma é: " + soma);
+    }
+}
