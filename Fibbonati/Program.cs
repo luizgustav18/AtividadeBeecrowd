9722083da354724019cc524ce33a8f626ba2c2c5
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-03-19 19:24:15 -0300

    commit

---
+++ Fibbonati/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um valor: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (i == N - 1)
+                Console.Write(a);
+            else
+                Console.Write(a + " ");
+
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ ListadeIeJ02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int I = 1; I <= 9; I += 2)
+        {
+            for (int J = I + 6; J >= I + 4; J--)
+            {
+                Console.WriteLine("I=" + I + " J=" + J);
+            }
+        }
+    }
+}
